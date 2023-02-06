0765d8d189198078b38166616b3bbc5d94c2f7d9
ishabansa <122788226+ishabansa@users.noreply.github.com>
2023-02-06 19:46:37 +0530

    Add files via upload

---
+++ Ifelseladder.java
@@ -0,0 +1,18 @@
+package controlstatement;
+
+public class Ifelseladder {
+	public static void main(String[] args) {
+		String name="Priya";
+		if(name=="Riya") {
+			System.out.println("Name is Riya");
+		}else if(name=="Neha") {
+			System.out.println("Name is Neha");
+		}else if(name=="Amit") {
+			System.out.println("Name is Amit");
+		}else {
+			System.out.println("Name");
+		}
+				
+	}
+
+}
