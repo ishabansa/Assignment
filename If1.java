6c95d5c5f7e94ce98e48e594965e17e970f55752
ishabansa <122788226+ishabansa@users.noreply.github.com>
2023-02-06 19:45:27 +0530

    Add files via upload

---
+++ If1.java
@@ -0,0 +1,16 @@
+package controlstatement;
+
+public class If1 {
+	public static void main(String[] args) {
+		int time = 22;
+		if(time<10) {
+			System.out.println("Good Morning.");
+		}
+		else if(time<18) {
+			System.out.println("Good Day.");
+		}else {
+			System.out.println("Good Evening.");
+		}
+	}
+
+}
