504ffea1a8d668abbe0690f5661c84d47fc28d38
ishabansa <122788226+ishabansa@users.noreply.github.com>
2023-02-08 20:21:59 +0530

    Add files via upload

---
+++ Face.java
@@ -0,0 +1,12 @@
+package Assignment;
+
+public class Face {
+	public static void main(String[] args) {
+		System.out.println("+\"\"\"\"\"+");
+		System.out.println("[|oo|]");
+		System.out.println(" | ^ |");
+		System.out.println("  |'-'|");
+		System.out.println("  +-----+");
+	}
+
+}
