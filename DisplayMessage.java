1a02f272a96cdb80ca35a86379013e04158793de
ishabansa <122788226+ishabansa@users.noreply.github.com>
2023-02-15 16:42:39 +0530

    Introduction To Java Hands_Activity_2

---
+++ DisplayMessage.java
@@ -0,0 +1,21 @@
+package com.cognizant.shapes;
+
+public class DisplayMessage {
+	
+		void printMessage() {
+			System.out.println("Hello My Message");
+		}
+		}
+		class MyProgram
+		{
+			public static void main(String[] args) {
+				DisplayMessage program=new DisplayMessage();
+				
+				program.printMessage();
+				}
+		}
+
+
+
+
+
